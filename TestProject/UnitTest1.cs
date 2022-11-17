39fa1dd213a3975300e2d696f319fc958839acf9
Arcady Barenboim <Arcady.Barenboim@ideo-digital.com>
2022-11-17 02:53:35 +0200

    test works

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -4,6 +4,7 @@ using PandologicJobs.Controllers;
 using PandologicJobs.Models;
 using PandologicJobs.Services;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,6 +13,7 @@ namespace TestProject
     public class JobsControllerTest
     {
         JobsController _controller;
+        private readonly Mock<IDataAccessService> _mockDataService;
         IDataAccessService _service;
         ILogger<DataAccessService> _loggerDa;
         ILogger<JobsController> _loggerJc;
@@ -21,14 +23,16 @@ namespace TestProject
             //or use this short equivalent 
             _loggerJc = Mock.Of<ILogger<JobsController>>();
             _loggerDa = Mock.Of<ILogger<DataAccessService>>();
+            _mockDataService = new Mock<IDataAccessService>();
             _service = new DataAccessService(_loggerDa);
-            _controller = new JobsController(_service, _loggerJc);
+            _controller = new JobsController(_mockDataService.Object, _loggerJc);
 
         }
         [Fact]
         public async Task GetJobsTestAsync()
         {
-
+            _mockDataService.Setup(repo => repo.GetPageGobs(1))
+                .Returns(Task.FromResult(new GChartDataTable() ));
             var result = await _controller.Get(1);
             //Assert
 
